7ec5e1a153d344cdc7013d3d1b4931e56976e4fa
tokuhirom <tokuhirom@gmail.com>
2013-08-11 09:12:17 +0900

    Import skelton

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Perl6::PVIP - It's new $module
+
+# SYNOPSIS
+
+    use Perl6::PVIP;
+
+# DESCRIPTION
+
+Perl6::PVIP is ...
+
+# LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tokuhirom <tokuhirom@gmail.com>
---
+++ lib/Perl6/PVIP.pm
@@ -0,0 +1,39 @@
+package Perl6::PVIP;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Perl6::PVIP - It's new $module
+
+=head1 SYNOPSIS
+
+    use Perl6::PVIP;
+
+=head1 DESCRIPTION
+
+Perl6::PVIP is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
